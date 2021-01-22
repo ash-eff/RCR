6de7f4b5c4006f90139d08e053e2d09f6bd21946
ash <ash.s.fischer@gmail.com>
2021-01-22 13:46:52 -0600

    Starting to add hazards

---
+++ Assets/Scripts/Player/PlayerHazardTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Server;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+public class PlayerHazardTrigger : MonoBehaviour
+{
+    [SerializeField] private Collider2D fallTrigger;
+    [SerializeField] private Hazard hazard = null;
+    [SerializeField] private float hazardGracePeriod;
+    private bool inHazard = false;
+    
+    public UnityEvent OnInHazardEvent;
+    public UnityEvent OnOutOfHazardEvent;
+
+    public Hazard GetCurrentHazard => hazard;
+
+    private void Awake()
+    {
+        if (OnInHazardEvent == null) OnInHazardEvent = new UnityEvent();
+        if (OnOutOfHazardEvent == null) OnOutOfHazardEvent = new UnityEvent();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Hazard"))
+            if (!inHazard)
+            {
+                hazard = other.GetComponent<Hazard>();
+                inHazard = true;
+                StartCoroutine(HazardGracePeriodTimer());
+            }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Hazard"))
+        {
+            hazard = null;
+            inHazard = false;
+            OnOutOfHazardEvent.Invoke();
+        }
+    }
+
+    IEnumerator HazardGracePeriodTimer()
+    {
+        yield return new WaitForSeconds(hazardGracePeriod);
+        if (inHazard)
+            OnInHazardEvent.Invoke();
+    }
+}
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -18,6 +18,7 @@ public class PlayerManager : MonoBehaviour
 	public StateMachine<PlayerManager> stateMachine;
 	[SerializeField] private float runSpeed = 40f;
 	private PlayerAnimationController animController;
+	private PlayerHazardTrigger hazardTrigger;
 	private PlayerController playerController;
 	private PlayerInput playerInput;
 	private int inventoryIndex = -1;
@@ -26,12 +27,15 @@ public class PlayerManager : MonoBehaviour
 	[SerializeField] private Weapon[] allWeapons;
 	[SerializeField] private Image weaponImage;
 	[SerializeField] private TextMeshProUGUI ammoText;
+	[SerializeField] private TextMeshProUGUI damageText;
+
 	private Dictionary<string, Weapon> availableWeaponsDictionary = new Dictionary<string, Weapon>();
 	private Dictionary<string, Weapon> ownedWeaponsDictionary = new Dictionary<string, Weapon>();
 
 
 	[NonSerialized] public readonly PlayerBaseState playerBaseState = new PlayerBaseState();
 	[NonSerialized] public readonly PlayerIdleState playerIdleState = new PlayerIdleState();
+
 
 	private void Awake()
 	{
@@ -42,6 +46,7 @@ public class PlayerManager : MonoBehaviour
 		animController = GetComponent<PlayerAnimationController>();
 		playerController = GetComponent<PlayerController>();
 		playerInput = GetComponent<PlayerInput>();
+		hazardTrigger = GetComponentInChildren<PlayerHazardTrigger>();
 		stateMachine = new StateMachine<PlayerManager>(this);
 		stateMachine.ChangeState(playerBaseState);
 	}
@@ -55,33 +60,13 @@ public class PlayerManager : MonoBehaviour
 		stateMachine.Update();
 	}
 	
-	void FixedUpdate ()
+	private void FixedUpdate ()
 	{
 		playerController.Move(playerInput.GetDirectionAxis * (runSpeed * Time.fixedDeltaTime));
 		stateMachine.FixedUpdate();
 	}
-	
-	public void CollectNewWeapon(Weapon weapon)
-	{
-		// if you already have the weapon, do nothing
-		if (ownedWeaponsDictionary.ContainsKey(weapon.name))
-			return;
 
-		// pull weapon from the available inventory of weapons
-		Weapon newWeapon = availableWeaponsDictionary[weapon.name];
-		newWeapon.gunPosition = newWeapon.transform.localPosition;
-		availableWeaponsDictionary.Remove(weapon.name);
-
-		// add to dictionary
-		ownedWeaponsDictionary.Add(newWeapon.name, newWeapon);
-		Debug.Log(newWeapon.name + " added to Inventory");
-		// add to inventory
-		MakeWeaponAvailableInInventory(newWeapon);
-		
-		// if it's the first weapon you pick up, automatically equip it
-		if(ownedWeaponsDictionary.Count == 1)
-			EquipWeapon(newWeapon);
-	}
+	#region Private Functions
 
 	private void EquipWeapon(Weapon weapon)
 	{
@@ -96,6 +81,52 @@ public class PlayerManager : MonoBehaviour
 		ammoText.text = ammo;
 		weaponImage.sprite = currentWeapon.artwork;
 	}
+
+	private void MakeWeaponAvailableInInventory(Weapon weapon)
+	{
+		weapon.isOwned = true;
+	}
+	
+	private void PositionWeaponHolder(float xDir, float rot)
+	{
+		// do nothing if there is no weapon equipped
+		if (currentWeapon == null)
+			return;
+		
+		var weaponTransform = currentWeapon.transform;
+		var weaponGunPosition = currentWeapon.gunPosition;
+		
+		// put weapon to the right or left of player based on cursor position
+		if (xDir > 0)
+		{
+			weaponTransform.localPosition = new Vector3(weaponGunPosition.x, weaponGunPosition.y, 0f);
+			weaponTransform.localScale = new Vector3(1, 1, 1);
+		}
+		if (xDir < 0)
+		{
+			weaponTransform.localPosition = new Vector3(-weaponGunPosition.x, weaponGunPosition.y, 0f);
+			weaponTransform.localScale = new Vector3(1, -1, 1);
+		}
+
+		// rotate the weapon
+		weaponTransform.rotation = Quaternion.Euler(0,0, rot);
+		
+		
+		// set the sprite order, behind or in front of the player based on cursor position
+		if (rot > 0f && rot < 180f)
+		{
+			currentWeapon.gunSprite.sortingOrder = 3;
+		}
+		else
+		{
+			currentWeapon.gunSprite.sortingOrder = 5;
+		}
+	}
+
+	#endregion
+	
+
+	#region Public Functions
 
 	public void SwapWeapon()
 	{
@@ -124,45 +155,26 @@ public class PlayerManager : MonoBehaviour
 		}
 	}
 	
-	private void MakeWeaponAvailableInInventory(Weapon weapon)
+	public void CollectNewWeapon(Weapon weapon)
 	{
-		weapon.isOwned = true;
-	}
-	
-	public void PositionWeaponHolder(float xDir, float rot)
-	{
-		// do nothing if there is no weapon equipped
-		if (currentWeapon == null)
+		// if you already have the weapon, do nothing
+		if (ownedWeaponsDictionary.ContainsKey(weapon.name))
 			return;
-		
-		var weaponTransform = currentWeapon.transform;
-		var weaponGunPosition = currentWeapon.gunPosition;
-		
-		// put weapon to the right or left of player based on cursor position
-		if (xDir > 0)
-		{
-			weaponTransform.localPosition = new Vector3(weaponGunPosition.x, weaponGunPosition.y, 0f);
-			weaponTransform.localScale = new Vector3(1, 1, 1);
-		}
-		if (xDir < 0)
-		{
-			weaponTransform.localPosition = new Vector3(-weaponGunPosition.x, weaponGunPosition.y, 0f);
-			weaponTransform.localScale = new Vector3(1, -1, 1);
-		}
 
-		// rotate the weapon
-		weaponTransform.rotation = Quaternion.Euler(0,0, rot);
+		// pull weapon from the available inventory of weapons
+		Weapon newWeapon = availableWeaponsDictionary[weapon.name];
+		newWeapon.gunPosition = newWeapon.transform.localPosition;
+		availableWeaponsDictionary.Remove(weapon.name);
+
+		// add to dictionary
+		ownedWeaponsDictionary.Add(newWeapon.name, newWeapon);
+		Debug.Log(newWeapon.name + " added to Inventory");
+		// add to inventory
+		MakeWeaponAvailableInInventory(newWeapon);
 		
-		
-		// set the sprite order, behind or in front of the player based on cursor position
-		if (rot > 0f && rot < 180f)
-		{
-		    currentWeapon.gunSprite.sortingOrder = 3;
-		}
-		else
-		{
-			currentWeapon.gunSprite.sortingOrder = 5;
-		}
+		// if it's the first weapon you pick up, automatically equip it
+		if(ownedWeaponsDictionary.Count == 1)
+			EquipWeapon(newWeapon);
 	}
 
 	//public void PlayerIsFalling(Vector3 atPosition)
@@ -191,4 +203,23 @@ public class PlayerManager : MonoBehaviour
 	//		Debug.Log(foo + " Falling.");
 	//	}
 	//}
+
+	public void DealWithHazard()
+	{
+		Hazard hazard = hazardTrigger.GetCurrentHazard;
+		if(hazard != null)
+			damageText.text = "Taking " + hazard.typeOfHazard + " damage!";
+		else
+		{
+			Debug.Log("No Hazard");
+		}
+	}
+
+	public void DealWithHazardExit()
+	{
+		damageText.text = "No hazard.";
+	}
+
+	#endregion
+
 }
